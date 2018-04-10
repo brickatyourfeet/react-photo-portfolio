1397520fc4fc69d6fe5a73081cabd3568f14734e
brickatyourfeet <brickatyourfeet@gmail.com>
2018-04-10 12:54:42 -0700

    fix prop-types to newer version with npm

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,26 +2,40 @@ import React, {Component} from 'react'
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 
-const posts = [{
-    id: '0',
-    description: 'test',
-    imgURL: "https://cdn3.digitalartsonline.co.uk/cmsdata/slideshow/3652838/rick-and-morty-art.jpg"
-},{
-    id: '1',
-    description: 'test2',
-    imgURL: "http://www.indiewire.com/wp-content/uploads/2017/07/rick-and-morty.png?w=780"
-},{
-    id: '2',
-    description: 'test3',
-    imgURL: "https://cdn-images-1.medium.com/max/900/1*cBie_JM1QFY78KfvOw2GoQ.png"
-}]
-
 
 class Main extends Component {
+    constructor(){
+        super()
+        this.state = {
+            posts: [{
+                id: '0',
+                description: 'test',
+                imgURL: "https://cdn3.digitalartsonline.co.uk/cmsdata/slideshow/3652838/rick-and-morty-art.jpg"
+            },{
+                id: '1',
+                description: 'test2',
+                imgURL: "http://www.indiewire.com/wp-content/uploads/2017/07/rick-and-morty.png?w=780"
+            },{
+                id: '2',
+                description: 'test3',
+                imgURL: "https://cdn-images-1.medium.com/max/900/1*cBie_JM1QFY78KfvOw2GoQ.png"
+            }]
+            
+        }
+        this.removePhoto = this.removePhoto.bind(this)
+    }
+
+    removePhoto(postRemoved){
+        console.log(postRemoved.description)
+        this.setState((state)=> ({
+            posts: state.posts.filter(post => post !== postRemoved)
+        }))
+    }
+
     render() {
         return <div>
             <Title title={'Photos'} />
-            <PhotoWall posts={posts}/>
+            <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} />
         </div>
     }
 }
